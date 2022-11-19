ca5bb43181a99372a5b6be05c5b64e4347f470c5
Lara Assunção Vieira <assuncaolara96@gmail.com>
2022-11-19 17:49:09 -0300

    Prova

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [numero, setNumero] = useState();
+  const [numeros, setNumeros] = useState([]);
+
+  function adicionar() {
+    let atual = [...numeros, numero];
+    setNumeros(atual);
+  }
+
+  function inverta() {
+    let atual = Inverter(numeros);
+    setNumeros(atual);
+  }
+
+   function Inverter(vetor) {
+    let atual = [];
+    let cont = 0
+    for (let i = vetor.length-1; i >= 0; i--){
+        atual[cont] = vetor[i];
+        cont++;
+    }
+    return atual;
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <h2> Função Adicionar Números </h2>
+        </div>
+        <div>
+          <input type='number' value={numero} onChange={ e => setNumero(e.target.value) } />
+          <button onClick={adicionar}> Adicione aqui </button>
+          <div>
+            <button onClick={inverta}> Inverter </button>
+          </div>
+        </div>
+        <div>
+          {
+            numeros.map(item =>
+              <p> {item} </p>
+              )
+          }
+        </div>
       </header>
     </div>
   );
